8c0a4dd9ecccfde59e3b08352d4b0093a4781c18
artem.miroshnichenko <artem.miroshnichenko@epicentrk.ua>
2024-03-14 09:33:59 +0200

    api samples

---
+++ api-samples/api.java
@@ -0,0 +1,27 @@
+public User getUser(String token) throws Exception {
+ final String domain = "https://core-api-qa.epicentrm.com.ua";
+ final String path = "/v1/my/user";
+ UriComponentsBuilder builder = UriComponentsBuilder
+   .fromHttpUrl(domain)
+   .path(path);
+ String url = builder.build().toUriString();
+
+ HttpHeaders headers = new HttpHeaders();
+ headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+ headers.setContentType(MediaType.APPLICATION_JSON);
+ headers.setBearerAuth(token);
+
+ HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
+ ResponseEntity<User> response;
+ try {
+  response = restTemplate.exchange(
+    url,
+    HttpMethod.GET,
+    httpEntity,
+    new ParameterizedTypeReference<User>() {});
+ } catch (Exception e) {
+  throw new Exception("Error while receiving user");
+ }
+
+ return response.getBody();
+}
